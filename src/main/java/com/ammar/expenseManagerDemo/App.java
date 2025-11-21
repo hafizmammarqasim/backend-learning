0c2975f0609dc3c211c7c239e137e51b71273116
Ammar <ammarhafiz222@gmail.com>
2025-11-21 23:51:26 +0500

    Initial commit till Spring XML

---
+++ src/main/java/com/ammar/expenseManagerDemo/App.java
@@ -0,0 +1,26 @@
+package com.ammar.expenseManagerDemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        //This is Manual Object Creation
+//        Dev dev = new Dev();
+//        dev.compile();
+
+        //What if we want to use Spring
+        //Use Dependency of Spring Context
+
+
+        //ApplicationContext is interface, so can't be used directly
+        //Use the class which implements Application Context
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Dev devWithSpring = (Dev) context.getBean("dev");
+
+        //Now we need to configure the xml file
+       // devWithSpring.compile();
+    }
+}
---
+++ src/main/java/com/ammar/expenseManagerDemo/Dev.java
@@ -0,0 +1,12 @@
+package com.ammar.expenseManagerDemo;
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev Initialized");
+    }
+
+    public void compile(){
+        System.out.println("Seekhny ki koshish");
+    }
+}
