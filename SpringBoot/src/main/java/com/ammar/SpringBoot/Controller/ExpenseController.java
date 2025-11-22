af38948ac8cb17fe5840870b928b077ea30896b8
Ammar <ammarhafiz222@gmail.com>
2025-11-22 22:47:15 +0500

    Applied CRUD operations without Database

--- SpringBoot/src/main/java/com/ammar/SpringBoot/Controller/ExpenseController.java
+++ SpringBoot/src/main/java/com/ammar/SpringBoot/Controller/ExpenseController.java
@@ -4,8 +4,7 @@ import com.ammar.SpringBoot.service.ExpenseService;
 import com.ammar.SpringBoot.model.Expense;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,29 @@ public class ExpenseController {
     @GetMapping("expenses")
     public List<Expense> getExpense(){
         return expenseService.getExpenseList();
+    }
+
+    // @PostMapping
+    // We use POST when we want to SEND data to the server (like submitting a form).
+    // @RequestBody
+    // This tells Spring: "Hey, don't look in the URL. Look inside the actual box (Body) of the
+    @PostMapping("expenses")
+    public void addExpense(@RequestBody Expense e){
+        expenseService.addExpense(e);
+    }
+
+    @GetMapping("expenses/{id}")
+    public Expense getExpenseById(@PathVariable int id){
+        return expenseService.getExpenseById(id);
+    }
+
+    @PutMapping("expenses")
+    public void updateExpense(@RequestBody Expense e){
+        expenseService.updateExpense(e);
+    }
+
+    @DeleteMapping("expenses/{expId}")
+    public void deleteExpenseById(@PathVariable int expId){
+        expenseService.deleteExpense(expId);
     }
 }
--- SpringBoot/src/main/java/com/ammar/SpringBoot/service/ExpenseService.java
+++ SpringBoot/src/main/java/com/ammar/SpringBoot/service/ExpenseService.java
@@ -3,21 +3,69 @@ package com.ammar.SpringBoot.service;
 import com.ammar.SpringBoot.model.Expense;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
-@Component
 public class ExpenseService {
 
-    List<Expense> expenseList = Arrays.asList(
+    //Arrays.asList creates a fixed size, so we wrapped it inside new ArrayList
+    List<Expense> expenseList = new ArrayList<>(Arrays.asList(
             new Expense(10, "Burger", 500),
-            new Expense(10,"Hair Cut",300),
-            new Expense(10, "Biryani", 500)
-            );
+            new Expense(11,"Hair Cut",300),
+            new Expense(12, "Biryani", 500)
+            ));
 
 
     public List<Expense> getExpenseList(){
         return expenseList;
+    }
+
+    public void addExpense(Expense e){
+        expenseList.add(e);
+    }
+
+    public Expense getExpenseById(int id){
+        for(Expense e:expenseList){
+            if(e.getExpId()==id){
+                System.out.println(e.getExpId()+ " "+e.getExpDesciption());
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public void updateExpense(Expense e){
+        //We don't have function for update, so we have to manually write logic
+        int index = findIndexById(e.getExpId());
+        //Check whether the expense exists?
+        if(index > -1){
+            //list.add() add the the product at that index and move others one step ahead
+            //expenseList.add(index,e);
+            expenseList.set(index,e);
+        }
+    }
+
+    public void deleteExpense(int id){
+        //We need index to delete by expenseList.remove() function
+        int index = findIndexById(id);
+
+        if(index > -1){
+            expenseList.remove(index);
+            System.out.println("Expense removed successfully");
+        }
+
+    }
+
+    //Helping function to find index of specific id
+    public int findIndexById(int id){
+
+        for(int i=0; i<expenseList.size(); i++){
+            if(expenseList.get(i).getExpId() == id)
+                return i;
+        }
+        return -1;
     }
 }
